7108aaf6a824f3a2321e2b6608dd44a465879cca
kotto5 <kotto555555@gmail.com>
2024-03-24 20:14:17 +0900

    prototype

--- solve_name.c
+++ solve_name.c
@@ -1,3 +1,30 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct s_maybeInt {
+    bool            ok;
+    unsigned        ip;
+}   t_maybeInt;
+
+static t_maybeInt solve_with_file(char *file) {
+    // TODO
+    return (t_maybeInt){false, 0};
+}
+
 // [Char] -> int
-int solve_name(char *host_name) {
+t_maybeInt    solve_name(char *host_name) {
+    t_maybeInt result = solve_with_file("/etc/hosts");
+    // TODO : solve with DNS? It maybe forbidden?
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2)
+        return -1;
+    t_maybeInt solve = solve_name(argv[1]);
+    if (solve.ok == false)
+        return -1;
+    else
+        printf("%d\n", solve.ip);
+    return 0;
 }
